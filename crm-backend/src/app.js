88fbb4bf76b7ae6367b0e0ea221145948f48a9b4
sarkhan <boy-ram@mail.ru>
2022-01-31 20:42:06 +0300

    all icons created via document createElement method

--- crm-backend/src/app.js
+++ crm-backend/src/app.js
@@ -13,14 +13,14 @@
       <circle cx="8" cy="8" r="8" fill="#9873FF"/>
       <path d="M11.56 9.50222C11.0133 9.50222 10.4844 9.41333 9.99111 9.25333C9.83556 9.2 9.66222 9.24 9.54222 9.36L8.84444 10.2356C7.58667 9.63556 6.40889 8.50222 5.78222 7.2L6.64889 6.46222C6.76889 6.33778 6.80444 6.16444 6.75556 6.00889C6.59111 5.51556 6.50667 4.98667 6.50667 4.44C6.50667 4.2 6.30667 4 6.06667 4H4.52889C4.28889 4 4 4.10667 4 4.44C4 8.56889 7.43556 12 11.56 12C11.8756 12 12 11.72 12 11.4756V9.94222C12 9.70222 11.8 9.50222 11.56 9.50222Z" fill="white"/>
       </g>`,
+    'other': `<g opacity="0.7">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM3 8C3 5.24 5.24 3 8 3C10.76 3 13 5.24 13 8C13 10.76 10.76 13 8 13C5.24 13 3 10.76 3 8ZM9.5 6C9.5 5.17 8.83 4.5 8 4.5C7.17 4.5 6.5 5.17 6.5 6C6.5 6.83 7.17 7.5 8 7.5C8.83 7.5 9.5 6.83 9.5 6ZM5 9.99C5.645 10.96 6.75 11.6 8 11.6C9.25 11.6 10.355 10.96 11 9.99C10.985 8.995 8.995 8.45 8 8.45C7 8.45 5.015 8.995 5 9.99Z" fill="#9873FF"/>
+      </g>`,
     'delete': `<g opacity="0.7">
       <path d="M8 2C4.682 2 2 4.682 2 8C2 11.318 4.682 14 8 14C11.318 14 14 11.318 14 8C14 4.682 11.318 2 8 2ZM8 12.8C5.354 12.8 3.2 10.646 3.2 8C3.2 5.354 5.354 3.2 8 3.2C10.646 3.2 12.8 5.354 12.8 8C12.8 10.646 10.646 12.8 8 12.8ZM10.154 5L8 7.154L5.846 5L5 5.846L7.154 8L5 10.154L5.846 11L8 8.846L10.154 11L11 10.154L8.846 8L11 5.846L10.154 5Z" fill="#F06A4D"/>
       </g>`,
     'change': `<g opacity="0.7">
       <path d="M2 11.5V14H4.5L11.8733 6.62662L9.37333 4.12662L2 11.5ZM13.8067 4.69329C14.0667 4.43329 14.0667 4.01329 13.8067 3.75329L12.2467 2.19329C11.9867 1.93329 11.5667 1.93329 11.3067 2.19329L10.0867 3.41329L12.5867 5.91329L13.8067 4.69329Z" fill="#9873FF"/>
-      </g>`,
-    'other': `<g opacity="0.7">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM3 8C3 5.24 5.24 3 8 3C10.76 3 13 5.24 13 8C13 10.76 10.76 13 8 13C5.24 13 3 10.76 3 8ZM9.5 6C9.5 5.17 8.83 4.5 8 4.5C7.17 4.5 6.5 5.17 6.5 6C6.5 6.83 7.17 7.5 8 7.5C8.83 7.5 9.5 6.83 9.5 6ZM5 9.99C5.645 10.96 6.75 11.6 8 11.6C9.25 11.6 10.355 10.96 11 9.99C10.985 8.995 8.995 8.45 8 8.45C7 8.45 5.015 8.995 5 9.99Z" fill="#9873FF"/>
       </g>`,
   }
 
@@ -83,10 +83,11 @@
     return container;
   }
 
-  function createContactIcon(elementType, linkClass = null) {
+  function createIcon(elementType, linkClass) {
     const type = ICON_STRINGS[elementType] === undefined ? 'other' : elementType;
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.classList.add('d-flex', 'svg-icon', linkClass);
+    svg.classList.add('d-flex', 'svg-icon');
+    if (linkClass !== undefined) svg.classList.add(linkClass);
     svg.setAttribute('width', '16');
     svg.setAttribute('height', '16');
     svg.setAttribute('fill', 'none');
@@ -169,19 +170,8 @@
       link.href = element.type === 'phone' ? ('tel:' + element.value) : (element.type === 'mail' ? ('mailto:' + element.value) : element.value);
       link.target = '_blank';
 
-      // const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-      const svg = createContactIcon(element.type, linkClass);
-      // svg.classList.add('d-flex','svg-icon', linkClass);
-      // svg.setAttribute('width', '16');
-      // svg.setAttribute('height', '16');
-      // svg.setAttribute('fill', 'none');
-      // svg.setAttribute('viewBox', '0 0 16 16');
-      // svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-      // svg.innerHTML =  element.type === 'phone' ? PHONE_ICON :
-      //                   element.type === 'VK' ? VK_ICON :
-      //                   element.type === 'mail' ? MAIL_ICON :
-      //                   element.type === 'FB' ? FB_ICON : OTHER_ICON;
+      const svg = createIcon(element.type, linkClass);
 
       link.append(svg);
       contactsDiv.append(link);
@@ -226,7 +216,7 @@
 
     const changeClientButton = document.createElement('a');
     changeClientButton.classList.add('d-inline-flex', 'd-flex', 'align-items-center', 'clients__list-change-button');
-    const changeClientIcon = document.createElement('a');
+    const changeClientIcon = createIcon('change');
     changeClientIcon.classList.add('d-inline-flex', 'change-element-icon');
     const changeClientText = document.createElement('span');
     changeClientText.classList.add('d-inline-flex');
@@ -235,7 +225,7 @@
 
     const deleteClientButton = document.createElement('a');
     deleteClientButton.classList.add('d-inline-flex', 'd-flex', 'align-items-center', 'clients__list-delete-button');
-    const deleteClientIcon = document.createElement('span');
+    const deleteClientIcon = createIcon('delete');
     deleteClientIcon.classList.add('d-inline-flex', 'delete-element-icon');
     const deleteClientText = document.createElement('span');
     deleteClientText.classList.add('d-inline-flex');
